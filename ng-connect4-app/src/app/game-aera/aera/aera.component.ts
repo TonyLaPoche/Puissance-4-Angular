abb5a037a17443338d730004e70ab3adff9d1e7a
tonylapoche <terrade.antoine.pro@gmail.com>
2023-04-28 16:57:22 +0200

    MAJ

--- ng-connect4-app/src/app/game-aera/aera/aera.component.ts
+++ ng-connect4-app/src/app/game-aera/aera/aera.component.ts
@@ -1,75 +1,80 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/auth.service';
-import { Color, Game, IsPlayed, Players, Round } from '../../game'
-import { GameService } from '../game.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/auth.service";
+import { Color, Game, IsPlayed, Players, Round } from "../../game";
+import { GameService } from "../game.service";
 @Component({
-  selector: 'app-aera',
+  selector: "app-aera",
   template: `
     <div class="game-container">
-
-    <div *ngIf="this.GameService.isWinner" id="myModal" class="modal" myModal>
-      <!-- Modal content -->
-      <div class="modal-content">
-        <span class="close" myModal>&times;</span>
-        <p class="modal-content-message">{{ currentRound.nextPlayerTurn.name }} win !</p>
-        <hr class="separator">
-        <div class="modal-content-btn">
-          <button class="btn" type="button" (click)="this.reload()">again ?</button>
-          <button class="btn" type="button" (click)="this.toGoHome()">new Challenger</button>
+      <div *ngIf="this.GameService.isWinner" id="myModal" class="modal" myModal>
+        <!-- Modal content -->
+        <div class="modal-content">
+          <span class="close" myModal>&times;</span>
+          <p class="modal-content-message">
+            {{ currentRound.nextPlayerTurn.name }} win !
+          </p>
+          <hr class="separator" />
+          <div class="modal-content-btn">
+            <button class="btn" type="button" (click)="this.reload()">
+              again ?
+            </button>
+            <button class="btn" type="button" (click)="this.toGoHome()">
+              new Challenger
+            </button>
+          </div>
         </div>
       </div>
-    </div>
 
       <div class="game-aera">
         <div class="game-zone">
           <div
             *ngFor="let zone of gameAeraClickable"
             class="game-zone-item"
-            (click)='GameService.isPlay(
+            (click)="
+              GameService.isPlay(
                 currentRound,
                 joueur1,
                 joueur2,
                 gameAera,
                 zone.cell,
                 currentRound.playerTurn.color
-              )'
-              appGame>
-          </div>
+              )
+            "
+            appGame
+          ></div>
         </div>
         <div class="game-aera-decoration">
           <div class="game-cell" *ngFor="let el of gameAera">
-            <div [ngClass]="[Klass, el.cell ]">
-            </div>
+            <div [ngClass]="[Klass, el.cell]"></div>
           </div>
         </div>
       </div>
     </div>
-  `
+  `,
 })
 export class AeraComponent implements OnInit {
-
   public joueur1: Players = { name: this.Login.players![0], color: Color.Red };
-  public joueur2: Players = { name: this.Login.players![1], color: Color.Yellow };
+  public joueur2: Players = {
+    name: this.Login.players![1],
+    color: Color.Yellow,
+  };
   public gameAera: Game[] = [];
 
   Klass: string = "game-jeton";
   gameAeraClickable: any[] = [];
 
-
   currentRound: Round = {
     playerTurn: this.joueur1,
     nextPlayerTurn: this.joueur2,
-    strokeLeft: 42
+    strokeLeft: 42,
   };
-
 
   constructor(
     public GameService: GameService,
     private Login: AuthService,
     private router: Router
-  ) {
-  }
+  ) {}
 
   ngOnInit() {
     this.initGame();
@@ -77,14 +82,14 @@ export class AeraComponent implements OnInit {
   }
 
   toGoHome() {
-    this.router.navigate(["/"])
-    this.initGame()
+    this.router.navigate(["/"]);
+    this.initGame();
     this.GameService.isWinner = false;
   }
 
   reload() {
-    this.gameAera = []
-    this.initGame()
+    this.gameAera = [];
+    this.initGame();
     this.GameService.isWinner = false;
   }
 
@@ -96,20 +101,18 @@ export class AeraComponent implements OnInit {
           id: `Row${r}Cell${c}`,
           row: `Row ${r}`,
           cellPos: { X: r, Y: c },
-          cell: IsPlayed.Empty
-        })
-
+          cell: IsPlayed.Empty,
+        });
       }
-
     }
-    return this.gameAera = newArray;
+    return (this.gameAera = newArray);
   }
 
   initClickableAera() {
     let array = this.gameAeraClickable;
     for (let i = 1; i <= 7; i++) {
-      array.push({ id: `zone ${i}`, cell: i })
+      array.push({ id: `zone ${i}`, cell: i });
     }
-    return this.gameAeraClickable = array;
+    return (this.gameAeraClickable = array);
   }
 }
